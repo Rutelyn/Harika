386995eb54209548dbc55c052bbec3be8c63ca8f
Harika <ruth.agbozo.93@gmail.com>
2024-06-18 13:30:43 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+const btn = document.getElementById('logo')
+btn.onclick = function () {
+    alert('Place your order')
+    
+}
+const btn2 = document.getElementById('menu')
+btn2.onclick = function () {
+    alert('More delicacies coming soon')
+}
